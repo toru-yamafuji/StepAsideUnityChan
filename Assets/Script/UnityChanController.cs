df2f674e5a6b778c58b66a3e44e3f41d5bd2a7a3
toru-yamafuji <samurai043@gmail.com>
2018-08-08 20:05:15 +0900

    second-commit

---
+++ Assets/Script/UnityChanController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+    private Animator myAnimator;
+
+    private Rigidbody myRigidbody;
+
+    private float forwardForce = 800.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        this.myAnimator = GetComponent<Animator>();
+
+        this.myAnimator.SetFloat("Speed", 1);
+
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+        Debug.Log(this.transform.forward);
+		
+	}
+}
