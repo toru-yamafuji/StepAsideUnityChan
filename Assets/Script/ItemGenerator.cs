3d1aab7018fc5b035999f8e185cf6fbfab8332ef
toru-yamafuji <samurai043@gmail.com>
2018-08-10 22:59:14 +0900

    課題提出

--- Assets/Script/ItemGenerator.cs
+++ Assets/Script/ItemGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ItemGenerator : MonoBehaviour
 {
@@ -13,17 +14,13 @@ public class ItemGenerator : MonoBehaviour
     private int goalPos = 120;
     private float posRange = 3.4f;
     private float unitychanPosZ;
-    private bool itemGem = true;
+    private int[] itemGenJudge = new int[20];
+    private int iIndex = 0;
 
 
     // Use this for initialization
     void Start()
     {
-
-
-
-
-
     }
 
     // Update is called once per frame
@@ -33,58 +30,15 @@ public class ItemGenerator : MonoBehaviour
 
         for (int i = startPos; i < goalPos; i += 15)
         {
-            int num = Random.Range(1, 11);
 
-            if (num <= 2)
+            if (i <= unitychanPosZ + 50 && !itemGenJudge.Contains(i))
             {
-
-                //cone*5
-                for (float j = -1; j <= 1; j += 0.4f)
-                {
-                    if (i <= unitychanPosZ + 50 && ItemGen==true)
-                    {
-                        GameObject cone = Instantiate(conePrefab) as GameObject;
-                        cone.transform.position = new Vector3(posRange * j, cone.transform.position.y, i);
-                    }
-                }
+                itemGenJudge[iIndex] = itemGen(i);
+                iIndex++;
             }
-            else
-            {
-                //randomItem
-
-
-                for (int j = -1; j <= 1; j++)
-                {
-                    int item = Random.Range(1, 11);
-                    int offsetz = Random.Range(-5, 6);
-
-                    if (1 <= item && item <= 6)
-                    {
-
-                        if (i <= unitychanPosZ + 50)
-                        {
-                            GameObject car = Instantiate(carPrefab) as GameObject;
-                            car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetz);
-
-                        }
-                    }
-                    else if (7 <= item && item <= 9)
-                    {
-
-                        if (i <= unitychanPosZ + 50)
-                        {
-                            GameObject coin = Instantiate(coinPrefab) as GameObject;
-                            coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetz);
-                        }
-                    }
-                }
-
-            }
-
 
 
         }
-
 
         GameObject carBehind = GameObject.Find("CarPrefab(Clone)");
         GameObject coneBehind = GameObject.Find("TrafficConePrefab(Clone)");
@@ -95,17 +49,68 @@ public class ItemGenerator : MonoBehaviour
         if (carBehind.transform.position.z < camera.transform.position.z)
         {
             Destroy(carBehind);
+            Debug.Log("destoyCar");
         }
         if (coneBehind.transform.position.z < camera.transform.position.z)
         {
             Destroy(coneBehind);
+            Debug.Log("destoyCone");
+
         }
         if (coinBehind.transform.position.z < camera.transform.position.z)
         {
             Destroy(coinBehind);
+            Debug.Log("destoyCoin");
+
         }
 
     }
+
+    public int itemGen(int distanceZ)
+    {
+
+        int num = Random.Range(1, 11);
+        if (num <= 2)
+        {
+
+            //cone*5
+            for (float j = -1; j <= 1; j += 0.4f)
+            {
+
+                GameObject cone = Instantiate(conePrefab) as GameObject;
+                cone.transform.position = new Vector3(posRange * j, cone.transform.position.y, distanceZ);
+            }
+        }
+        else
+        {
+            //randomItem
+
+
+            for (int j = -1; j <= 1; j++)
+            {
+                int item = Random.Range(1, 11);
+                int offsetz = Random.Range(-5, 6);
+
+                if (1 <= item && item <= 6)
+                {
+
+                    GameObject car = Instantiate(carPrefab) as GameObject;
+                    car.transform.position = new Vector3(posRange * j, car.transform.position.y, distanceZ + offsetz);
+
+                }
+                else if (7 <= item && item <= 9)
+                {
+
+                    GameObject coin = Instantiate(coinPrefab) as GameObject;
+                    coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, distanceZ + offsetz);
+                }
+            }
+
+        }
+        return distanceZ;
+
+    }
+
 
 
 
